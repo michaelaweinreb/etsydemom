6a8436018514a89f4ca30e844605800d0e4ca776
michaelaweinreb <michaela.weinreb@hotmail.com>
2016-06-25 00:27:39 +0200

    add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622201946) do
+ActiveRecord::Schema.define(version: 20160624214509) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
