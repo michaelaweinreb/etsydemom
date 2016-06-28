74a5d8fbb737d4f206cc7da9681a7496ebc15bac
michaelaweinreb <michaela.weinreb@hotmail.com>
2016-06-28 17:18:00 +0200

    add validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,13 @@
 class Listing < ActiveRecord::Base
     has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "DeathtoStock_Wired2.jpg"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+    belongs_to :user
+    has_many :orders
+    validates :name, :description, :price, presence: true
+    validates :price, numericality: {greater_than: 0}
+    validates_attachment_presence :image
+
 end
+
+ 
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624223529) do
+ActiveRecord::Schema.define(version: 20160626213835) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 20160624223529) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "listing_id"
+    t.integer  "buyer_id"
+    t.integer  "seller_id"
   end
 
   create_table "users", force: :cascade do |t|
